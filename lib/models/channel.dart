71796e35a3d8a18d1f3bef0ebc55df4cb1ffe81d
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-07-29 16:22:43 +0530

    channel done

--- lib/models/channel.dart
+++ lib/models/channel.dart
@@ -10,8 +10,7 @@ class Channel extends Exception {
   late ChannelType type;
   late String server;
   late List<String> memberlist;
-  late String category;
-   create_channel(
+  create_channel(
     Database db2,
     Database db3,
     StoreRef<String, Map> channel_store,
@@ -21,10 +20,9 @@ class Channel extends Exception {
     var server_record,
   ) async {
     //check if the user has logged in or not
-
-    if (await super.logged_in(user1)) {
+    if(await super.logged_in(user1)){
       return;
-    } else {
+    }
       //taking input which server you want your channel to be in
 
       stdout.write("which server you want to join? -> ");
@@ -35,25 +33,39 @@ class Channel extends Exception {
       stdout.write("Enter the Channel Name:");
       final channelName = stdin.readLineSync() as String;
       this.name = channelName;
-      var c_record = await server_store.find(db3);
-      for (var rec in c_record) {
-        if (rec.key == channelName && rec.value['server_name'] == serverName) {
-          for (var user in rec.value['memberlist']) {
-            if (user == user1.username) {
-              print("User is already in the channel");
-              return;
-            }
-          }
-          Map channel_list =
-              await channel_store.record(channelName).get(db3) as Map;
-          channel_list = cloneMap(channel_list); //Create a copy of the map
-          channel_list['memberlist'].add(user1.username);
-          await channel_store.record(channelName).delete(db3);
-          await channel_store.record(channelName).put(db3, channel_list);
-          print("Channel added successfully");
+      stdout.write("Channel Type:[text/voice/announcement/hackathons]: ");
+      final channeltype = stdin.readLineSync() as String;
+
+      switch (channeltype) {
+        case "text":
+          this.type = ChannelType.text;
+          break;
+        case "voice":
+          this.type = ChannelType.text;
+          break;
+        case "announcements":
+          this.type = ChannelType.text;
+          break;
+        case "hackathons":
+          this.type = ChannelType.text;
+          break;
+        default:
+          print("Invalid input");
           return;
-        }
       }
-    }
+      Map<String, dynamic> s_map = {
+        'server_name': serverName,
+        'mememberlist': [user1.username],
+        'type': channeltype,
+      };
+      await channel_store.record(channelName).put(db3, s_map);
+      //for adding channel in server.db if category is null
+      Map pr = await server_store.record(serverName).get(db2) as Map;
+      pr = cloneMap(pr); // Create a copy of the map
+      pr['channellist'].add(channelName);
+      await server_store.record(serverName).delete(db2);
+      await server_store.record(serverName).put(db2, pr);
+      print('User added to the channel successfully');
+    
   }
 }
